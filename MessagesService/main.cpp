85dd822b69524e11e6bcabf4b3c5385960e94243
Adeon18 <ostaptrush18@gmail.com>
2023-02-12 13:52:47 +0200

    Connected the libhttp and implemented basic MessageService

--- MessagesService/main.cpp
+++ MessagesService/main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include <httpserver.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+static constexpr int SERVICE_PORT = 8082;
+
+using namespace httpserver;
+
+class MessageServiceResource : public http_resource {
+    inline static std::string GET_RESPONSE = "Message server GET response placeholder";
+public:
+    MessageServiceResource() {
+        disallow_all();
+        set_allowing("GET", true);
+    }
+    std::shared_ptr<http_response> render_GET(const http_request&) override {
+        return std::shared_ptr<http_response>(new string_response(GET_RESPONSE));
+    }
+};
+
+int main(int argc, char** argv) {
+    webserver ws = create_webserver(SERVICE_PORT);
+
+    MessageServiceResource hwr{};
+    ws.register_resource("/MessagesService", &hwr);
+    ws.start(true);
+
     return 0;
 }
