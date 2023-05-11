5fffba89c60bc9765d903ae99c847f9d89a64f54
Adeon18 <ostaptrush18@gmail.com>
2023-05-11 12:18:37 +0300

    Connected the consul lib lol, let's see if I can do something here...

--- FacadeService/main.cpp
+++ FacadeService/main.cpp
@@ -1,8 +1,26 @@
 #include "Controller/FacadeController.hpp"
 
+#include "ppconsul/agent.h"
+
+using ppconsul::Consul;
+using namespace ppconsul::agent;
+
 namespace hs = httpserver;
 
 int main(int argc, char** argv) {
+
+    // Create a consul client that uses default local endpoint `http://127.0.0.1:8500` and default data center
+    Consul consul;
+// We need the 'agent' endpoint for a service registration
+    Agent agent(consul);
+
+// Register a service with associated HTTP check:
+    agent.registerService(
+            kw::name = "my-service",
+            kw::port = 9876,
+            kw::tags = {"tcp", "super_server"},
+            kw::check = HttpCheck{"http://localhost:80/", std::chrono::seconds(2)}
+    );
 
     bool blocking = true;
     hs::webserver facadeServer = hs::create_webserver(8080);
